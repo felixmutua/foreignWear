0d9e1991174e06be8d075432748fdbe0979730bc
Felix Mutua <felixmutua01@gmail.com>
2017-10-07 12:21:15 +0300

    week 1 progress

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  WooCommerce: any;
+  products: any[];
 
   constructor(public navCtrl: NavController) {
+
+    this.WooCommerce =WC({
+      url:"http://localhost/wordpress",
+      consumerKey: "ck_8d3cc59b0b0d6dc2f30202dbcf9997ca1f5e859c",
+      consumerSecret: "cs_00b296fd1ef72c84dff53302a9e3d66948cbc883"
+
+    });
+    this.WooCommerce.getAsync("products").then( (data) =>{
+      console.log(JSON.parse(data.body));
+      this.products = JSON.parse(data.body).products;
+    },(err)=>{
+      console.log(err)   
+
+    }) 
 
   }
 
