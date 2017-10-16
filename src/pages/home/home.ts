39a1b8e95dafe8fbbbff5a75cde8b683aa7bcaa4
Felix Mutua <felixmutua01@gmail.com>
2017-10-16 16:47:22 +0300

    Push 3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides, ToastController } from 'ionic-angular';
 
 import * as WC from 'woocommerce-api';
 
@@ -10,8 +10,13 @@ import * as WC from 'woocommerce-api';
 export class HomePage {
   WooCommerce: any;
   products: any[];
+  moreProducts: any[];
+  page:number;
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild ('productSlides') productSlides: Slides;
+
+  constructor(public navCtrl: NavController,  public toastCtrl: ToastController) {
+    this.page =2;
 
     this.WooCommerce =WC({
       url:"http://localhost/wordpress",
@@ -19,6 +24,8 @@ export class HomePage {
       consumerSecret: "cs_00b296fd1ef72c84dff53302a9e3d66948cbc883"
 
     });
+
+    this.loadMoreProducts(null);
     this.WooCommerce.getAsync("products").then( (data) =>{
       console.log(JSON.parse(data.body));
       this.products = JSON.parse(data.body).products;
@@ -26,6 +33,49 @@ export class HomePage {
       console.log(err)   
 
     }) 
+
+  }
+
+  ionViewDidLoad(){
+    setInterval(()=>{
+      if(this.productSlides.getActiveIndex() == this.productSlides.length() -1)
+      this.productSlides.slideTo(0);
+
+
+      this.productSlides.slideNext();  
+
+    },3000)
+  }
+  
+  loadMoreProducts(event){
+
+    if(event ==null){
+      this.page=2;
+      this.moreProducts=[];
+    }
+    
+    else
+    this.page++;
+    this.WooCommerce.getAsync("products?page=" + this.page).then( (data) =>{
+      console.log(JSON.parse(data.body));
+      this.moreProducts = this.moreProducts.concat(JSON.parse(data.body).products);
+
+      if(event!=null) {
+        event.complete();
+
+        if(JSON.parse(data.body).products.length <10){
+          event.enable(false);
+
+          this.toastCtrl.create({
+            message: "No More Products!",
+            duration: 5000
+          }).present();
+        }
+      }
+    },(err)=>{
+      console.log(err)   
+
+    })  
 
   }
 
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
+import * as WC from 'woocommerce-api';
+
+
  
 @Component({
   selector: 'page-menu',
@@ -9,9 +12,55 @@ import { HomePage } from '../home/home';
 })
 export class MenuPage {
   homePage: Component;
+  WooCommerce: any;
+  categories: any[];
+  
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.homePage = HomePage;
-  }
+    this.categories=[];
+
+    this.WooCommerce =WC({
+      url:"http://localhost/wordpress",
+      consumerKey: "ck_8d3cc59b0b0d6dc2f30202dbcf9997ca1f5e859c",
+      consumerSecret: "cs_00b296fd1ef72c84dff53302a9e3d66948cbc883"
+
+    });
+
+
+    this.WooCommerce.getAsync("products/categories").then( (data) =>{
+      console.log(JSON.parse(data.body).product_categories);
+
+      let temp: any[]=JSON.parse(data.body).product_categories;
+
+      for(let i=0;i<temp.length;i++){
+        if(temp[i].parent==0){
+
+          if(temp[i].slug=="accessories"){
+            temp[i].icon="key";
+          }
+          if(temp[i].slug=="music"){
+            temp[i].icon="musical-notes";
+          }
+          if(temp[i].slug=="clothing"){
+            temp[i].icon="shirt";
+          }
+          if(temp[i].slug=="shoes"){
+            temp[i].icon="cash";
+          }
+          if(temp[i].slug=="bags"){
+            temp[i].icon="pricetag";
+          }
+          this.categories.push(temp[i]);
+        }
+      }
+ 
+    },(err)=>{
+      console.log(err)   
+
+    }) 
+   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
